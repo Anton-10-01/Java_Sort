a0737b67f22d800780c0b9a56e96001901af11a8
Anton <razjivin.anton@yandex.ru>
2023-10-14 11:45:59 +0300

    Изменил кол-тво перебираемых объектов в цикле

--- DirectSort.java
+++ DirectSort.java
@@ -16,7 +16,7 @@ public class DirectSort {
     }
     
     public static void directSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             int minPosition = i;
             for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < array[minPosition]) {
