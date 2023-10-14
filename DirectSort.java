43b676bf74c1ffe06e73d9e88156ff9555913b51
Anton <razjivin.anton@yandex.ru>
2023-10-14 11:40:10 +0300

    add DirectSort.java

---
+++ DirectSort.java
@@ -0,0 +1,33 @@
+package Java.Sort;
+
+/**
+ * DirectSort
+ */
+public class DirectSort {
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {
+            4, 3, 6, 2, 10, 8, 7, 9, 5, 1
+    };
+    directSort(arr);
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(" " + arr[i]);
+    }
+    }
+    
+    public static void directSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minPosition = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minPosition]) {
+                    minPosition = j;
+                }
+            }
+            if (i != minPosition) {
+                int temp = array[i];
+                array[i] = array[minPosition];
+                array[minPosition] = temp;
+            }
+        }
+    }
+}
